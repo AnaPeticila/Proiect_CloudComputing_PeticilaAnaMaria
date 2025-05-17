7ed97d822423dc91dd7e2564a6f1c355c8528d4f
Peticila Ana Maria <anapeticila@gmail.com>
2025-05-17 21:43:26 +0300

    added record form title

--- components/RecordForm.jsx
+++ components/RecordForm.jsx
@@ -28,6 +28,9 @@ const RecordForm = ({ entry, onSubmit }) => {
     return (
         <div>
             <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+                <div className= "text-center font-bold text-xl">
+                    {entry?._id ? "Update record" : "Create record"}
+                </div>
                 <div className="mb-5">
                     <label
                         htmlFor="title"
