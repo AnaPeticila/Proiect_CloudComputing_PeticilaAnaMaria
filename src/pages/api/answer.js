db5555d22a2d774d891a95c39bc243a9f91f5f58
Peticila Ana Maria <anapeticila@gmail.com>
2025-05-18 20:50:58 +0300

    fix import

---
+++ src/pages/api/answer.js
@@ -0,0 +1,97 @@
+import { sendBadRequest, sendMethodNotAllowed } from "../../../utils/apiMethods";
+import openai from "../../../lib/openai";
+import { MAX_MEMORY } from "../../../utils/constants";
+
+const SYSTEM_PROMPTS = {
+    SIMPLE_ASSISTANT: {
+        MESSAGE: {
+            'role': 'system',
+            'content': 'You are a simple assistant. You respond with simple sentences.',
+        },
+        TEMPERATURE: 1,
+        MAX_TOKENS: 50,
+        TYPE: 'simple_assistant',
+    },
+    USER: {
+        MESSAGE: {
+            'role': 'user',
+            'content': 'You are a user. You respond with normal sentences.',
+        },
+        TEMPERATURE: 1,
+        MAX_TOKENS: 100,
+        TYPE: 'user',
+    },
+};
+
+const chatCompletion = async (messages, maxTokens, temperature) => {
+    const rawResponse = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        max_tokens: maxTokens,
+        temperature: temperature,
+
+    })
+
+    return rawResponse?.choices[0];
+}
+
+
+const converseChat = async (res, messages, role) => {
+    try {
+
+        let newMessages = [];
+
+        if (messages.length > MAX_MEMORY) {
+            newMessages = messages.slice(-MAX_MEMORY);
+        }
+        else {
+            newMessages = messages;
+        }
+
+        const messagesArray = [
+            role.MESSAGE,
+            ...newMessages
+        ];
+
+        const response = await chatCompletion(messagesArray, role.MAX_TOKENS, role.TEMPERATURE);
+
+        return sendOk(res, response);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+const converse = (res, messages, type) => {
+    switch (type) {
+        case SYSTEM_PROMPTS.SIMPLE_ASSISTANT.TYPE:
+            return converseChat(res, messages, SYSTEM_PROMPTS.SIMPLE_ASSISTANT);
+        case SYSTEM_PROMPTS.USER.TYPE:
+            return converseChat(res, messages, SYSTEM_PROMPTS.USER);
+        default:
+            return sendBadRequest(res, 'Wrong Conversation type!');
+    }
+}
+
+
+export default async function handler(req, res) {
+    const { method, body } = req;
+
+    const isAllowedMethods = req.method === 'POST';
+
+    if (!isAllowedMethods) {
+        return sendMethodNotAllowed(res, "Method not allowed");
+    }
+
+    const { messages, type } = body;
+
+    if (!messages || !type) {
+        return sendBadRequest(res, 'Bad request');
+    }
+    try {
+        return converse(res, messages, type);
+
+    } catch (error) {
+        console.log(error);
+
+    }
+}
---
+++ src/pages/chat.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ChatComponent from '../../components/ChatComponents';
+
+const chat = () => {
+    return (
+       <ChatComponent />
+    )
+}
+
+export default chat;
